c5971f8c1849c08b2a865b3f5d5508695773ad59
Youssef Samy <office.youssefkassem@gmail.com>
2026-04-24 11:50:02 +0300

    Fix test -f canary problem

--- patch-build.sh
+++ patch-build.sh
@@ -54,9 +54,9 @@ fi
 
 sedi '/^:: Validate/,$d' "$BUILD_STEPS_WIN"
 echo "✅ Truncated $BUILD_STEPS_WIN at ':: Validate'"
-sedi '/^[[:space:]]*( startgroup "Validating outputs" )/,$d' $BUILD_STEPS_LINUX
+sedi '/^[[:space:]]*( startgroup "Validating outputs" )/,/^[[:space:]]*( endgroup "Validating outputs" )/d' $BUILD_STEPS_LINUX
 echo "fi" >> $BUILD_STEPS_LINUX
 echo "✅ Truncated $BUILD_STEPS_LINUX at 'Validating outputs'"
-sedi '/^[[:space:]]*( startgroup "Validating outputs" )/,$d' $BUILD_STEPS_OSX
+sedi '/^[[:space:]]*( startgroup "Validating outputs" )/,/^[[:space:]]*( endgroup "Validating outputs" )/d' $BUILD_STEPS_OSX
 echo "fi" >> $BUILD_STEPS_OSX
 echo "✅ Truncated $BUILD_STEPS_OSX at 'Validating outputs'"
