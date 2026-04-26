7207589caae360903e9f1bb4a67b7e4c064c512c
Youssef Samy <office.youssefkassem@gmail.com>
2026-04-26 22:43:23 +0300

    Testing without pkgconfig fix on linux

--- patch-build.sh
+++ patch-build.sh
@@ -12,7 +12,6 @@ perl -i -pe 'print "    # GStreamer dependencies (added by patch)\n    - gstream
 echo "✅ Added GStreamer dependencies"
 
 echo "Patching $BUILD_FILE_UNIX"
-perl -i -pe 's|^export PKG_CONFIG_LIBDIR=\$PREFIX/lib|export PKG_CONFIG_PATH=\$PREFIX/lib/pkgconfig:\$PREFIX/share/pkgconfig:\$PKG_CONFIG_PATH|' "$BUILD_FILE_UNIX"
 if grep -q -- "-DWITH_GSTREAMER" "$BUILD_FILE_UNIX"; then
     perl -i -pe 's/-DWITH_GSTREAMER=\d+/-DWITH_GSTREAMER=1/g' "$BUILD_FILE_UNIX"
     echo "✅ Updated -DWITH_GSTREAMER to =1"
