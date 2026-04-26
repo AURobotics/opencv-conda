f9fb6f86eb162a8de52651261a5bb61f0f6d11d5
Youssef Samy <office.youssefkassem@gmail.com>
2026-04-26 21:19:01 +0300

    the actual actual actual newline fix

--- patch-workflow.sh
+++ patch-workflow.sh
@@ -42,12 +42,12 @@ OSX_MATRIX=$(echo "$OSX_MATRIX" | perl -pe '
 ')
 
 # Combine matrices
-COMBINED_MATRIX="${WINDOWS_MATRIX}${OSX_MATRIX}"
+COMBINED_MATRIX="${WINDOWS_MATRIX}"$'\n'"${OSX_MATRIX}"
 
 # Insert matrices after "        include:"
 echo "$COMBINED_MATRIX" | perl -i -pe '
     BEGIN { $matrix = do { local $/; <STDIN> }; }
-    if (/^\s+include:/) { $_ .= $matrix . "\n"; }
+    if (/^\s+include:/) { $_ .= $matrix; }
 ' "$WORKFLOW_FILE"
 
 # Set working directory
